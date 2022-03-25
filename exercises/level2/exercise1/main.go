e8370ed00cb1de2145d1209dd1c0e3273658cc0d
DiabeticOwl <jh.cruz91@gmail.com>
2022-03-25 15:00:12 -0400

    Initial Level 2

---
+++ exercises/level2/exercise1/main.go
@@ -0,0 +1,16 @@
+// Hands-on exercise #1
+// Write a program that prints a number in decimal, binary, and hex
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 86
+
+	fmt.Printf("Decimal: %d\n", x)
+	fmt.Printf("Binary: %b\n", x)
+	fmt.Printf("Hexadecimal: %#X\n", x)
+}
