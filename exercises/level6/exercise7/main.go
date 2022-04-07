02532000d55f6ed7b6ec095e848afe15ddced787
DiabeticOwl <jh.cruz91@gmail.com>
2022-04-07 19:09:48 -0400

    Level 7 - Exercise #7

---
+++ exercises/level6/exercise7/main.go
@@ -0,0 +1,16 @@
+// Hands-on exercise #7
+// ● Assign a func to a variable, then call that func
+
+// Note: I did this on the past exercise, lol
+
+package main
+
+import "fmt"
+
+func main() {
+	f2 := func() {
+		fmt.Println("Hello World! Again. :D")
+	}
+
+	f2()
+}
