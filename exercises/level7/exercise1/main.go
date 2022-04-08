8dc6e6e4fa1bafbfecc0fb9e0b6d769b0418fb6b
DiabeticOwl <jh.cruz91@gmail.com>
2022-04-07 22:16:07 -0400

    Level 7 - Exercise #1

---
+++ exercises/level7/exercise1/main.go
@@ -0,0 +1,12 @@
+// Hands-on exercise #1
+// ● Create a value and assign it to a variable.
+// ● Print the address of that value.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 6
+	fmt.Println(&x)
+}
